d05ee7fbc2c9f9b47a1c6c71577044cf16eea8f0
Wallaby Dingo <wallabydingo@me.com>
2022-08-14 22:17:20 +1000

    Completed and tested Enumerable#my_each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,14 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    i = 0
+    if block_given?
+      until i === self.length do
+        yield self[i], i
+        i += 1
+      end
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +16,14 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    i = 0
+    if block_given?
+      until i === self.length do
+        yield self[i]
+        i += 1
+      end
+    end
+    self
+  end
 end

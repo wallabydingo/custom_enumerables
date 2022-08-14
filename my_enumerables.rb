94bb3b3d10c09ef2c930f7fcdcc7e8a717713974
wallabydingo <79701625+wallabydingo@users.noreply.github.com>
2022-08-14 21:56:24 +1000

    Completed and passed test Array#my_each

---
+++ my_enumerables.rb
@@ -0,0 +1,20 @@
+module Enumerable
+  # Your code goes here
+end
+
+# You will first have to define my_each
+# on the Array class. Methods defined in
+# your enumerable module will have access
+# to this method
+class Array
+  def my_each
+    i = 0
+    if block_given?
+      until i === self.length do
+        yield self[i]
+        i += 1
+      end
+    end
+    self
+  end
+end
